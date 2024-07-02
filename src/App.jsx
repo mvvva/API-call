6b5671b3a4247d9a248dd1888bffd7c019f21788
saeed <mvvva001@gmail.com>
2024-07-02 22:15:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import User from './components/user'
+
+
+function App() {
+  return (
+    <>
+      <User/>
+    </>
+  )
+}
+
+export default App
